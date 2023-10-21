49ae0e77e2206987fd688aaacec36623727cdef1
duduardo13 <eduardofontes430@gmail.com>
2023-10-21 14:41:04 -0300

    operações basicas

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var resultado;
 var operador = "";
 var del;
 
+//preenchendo o visor 
 function preencher(numero) {
     var aux = document.getElementById("visor").value;
     if (aux == "0") {
@@ -16,6 +17,93 @@ function preencher(numero) {
   // Função para zerar o visor
 function zerar() {
     document.getElementById("visor").value = 0;
+    valor1 = "";
+    operador = "";
+  }
+
+
+  //Escolhendo as operações basicas
+  function operacao(sinal) {
+    if (valor1 == "") {
+      operador = sinal;
+      valor1 = parseFloat(document.getElementById("visor").value);
+      document.getElementById("visor").value = "";
+    } else {
+      switch (operador) {
+        case "+":
+          operador = sinal;
+          valor2 = document.getElementById("visor").value;
+          valor1 = parseFloat(valor1) + parseFloat(valor2);
+          document.getElementById("visor").value = valor1;
+          document.getElementById("visor").value = "";
+          valor2 = "";
+          break;
+        case "-":
+          operador = sinal;
+          valor2 = document.getElementById("visor").value;
+          valor1 = parseFloat(valor1) - parseFloat(valor2);
+          document.getElementById("visor").value = valor1;
+          document.getElementById("visor").value = "";
+          valor2 = "";
+          break;
+        case "/":
+          operador = sinal;
+          valor2 = document.getElementById("visor").value;
+          valor1 = parseFloat(valor1) / parseFloat(valor2);
+          document.getElementById("visor").value = valor1;
+          document.getElementById("visor").value = "";
+          valor2 = "";
+          break;
+        case "*":
+          operador = sinal;
+          valor2 = document.getElementById("visor").value;
+          valor1 = parseFloat(valor1) * parseFloat(valor2);
+          document.getElementById("visor").value = valor1;
+          document.getElementById("visor").value = "";
+          valor2 = "";
+          break;
+        default:
+          resultado = valor1;
+          break;
+      }
+    }
+  }
+  
+  //Calculando
+
+  function calcular() {
+    switch (operador) {
+      case "+":
+        resultado = valor1 + parseFloat(document.getElementById("visor").value);
+        document.getElementById("visor").value = resultado.toLocaleString(
+          "pt-BR"
+        );
+        break;
+      case "-":
+        resultado = valor1 - parseFloat(document.getElementById("visor").value);
+        document.getElementById("visor").value = resultado.toLocaleString(
+          "pt-BR"
+        );
+        break;
+      case "/":
+        resultado = valor1 / parseFloat(document.getElementById("visor").value);
+        document.getElementById("visor").value = resultado.toLocaleString(
+          "pt-BR"
+        );
+        break;
+      case "*":
+        resultado = valor1 * parseFloat(document.getElementById("visor").value);
+        document.getElementById("visor").value = resultado.toLocaleString(
+          "pt-BR"
+        );
+        break;
+      default:
+        resultado = valor1;
+        document.getElementById("visor").value = resultado.toLocaleString(
+          "pt-BR"
+        );
+        break;
+    }
     valor1 = "";
     operador = "";
   }
